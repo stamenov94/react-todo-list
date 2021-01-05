cfcdb458fcc2cfd022423fba514a7d2d311488ab
Kristiyan Stamenov <kristiyan@stamenov.net>
2021-01-05 22:06:40 +0200

    showing vs code github setup

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import uuid from 'uuid';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import TodoInput from './components/TodoInput';
 import TodoList from './components/TodoList';
+
+//showing vs code github setup
 class App extends Component {
   render() {
     return (
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Item from './TodoItem'
+
 
 export default class TodoItem extends Component {
     render() {
